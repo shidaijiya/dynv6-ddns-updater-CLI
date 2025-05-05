d16b4f64bffb9ac9506e8f56d08604fd5464235c
shidaijiya <shidaijiya@icloud.com>
2025-05-06 05:36:31 +0800

    pre v 0.1

---
+++ lib/log_utils.py
@@ -0,0 +1,25 @@
+import logging
+from pathlib import Path
+
+Path('./log').mkdir(parents=True, exist_ok=True)
+
+logging.basicConfig(
+    filename=f'./log/ddns.log',
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s: %(message)s',
+    filemode='w'
+)
+
+def log_print(msg,log_level="INFO"):
+    if log_level == "INFO":
+        logging.info(msg)
+        print(msg)
+    elif log_level == "WARNING":
+        logging.warning(msg)
+        print(msg)
+    elif log_level == "DEBUG":
+        logging.debug(msg)
+        print(msg)
+    elif log_level == "ERROR":
+        logging.error(msg)
+        print(msg)
